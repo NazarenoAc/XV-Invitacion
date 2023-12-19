ef1741dec658cbc63de26f9eaea5f11918e0bbf3
Nazareno <acv2naza@gmail.com>
2023-12-19 11:05:19 -0300

    Muestra

--- js/countdown.js
+++ js/countdown.js
@@ -16,8 +16,7 @@ simplyCountdown('#cuenta', {
     inlineClass: 'simply-countdown-inline', //inline css span class in case of inline = true
     // in case of inline set to false
     enableUtc: false, //Use UTC or not - default : false
-    onEnd: function() {
-        alert('Este evento ya finalizó.'); return; }, //Callback on countdown end, put your own function here
+    //Callback on countdown end, put your own function here
     refresh: 1000, // default refresh every 1s
     sectionClass: 'simply-section', //section css class
     amountClass: 'simply-amount', // amount css class
